25749d109d9fa592a37fbd0b55aa6d52322a0eb7
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2023-10-24 15:57:42 -0300

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,36 +1,50 @@
 @extends('layouts/main')
 
+<style>
+    .card-cardapio{
+        background: #ffffff96;
+    padding: 8px;
+    border-radius: 20px;
+    border-style: double;
+    margin-top: 65px;
+    }
+</style>
 
 @section('content')
 
-@foreach ($categories as $categorie)
-<div class="py-content">
-    <div>
-        <div class="text-center">
-            <h3 class="title-section text-uppercase category-title">{{ $categorie->name }}</h3>
-        </div>
-    </div>
-    <div class="row">
-        @foreach ($categorie->produtos as $produtos)
-        <div class="col-md-6">
-            <div class="product">
-                <div class="product-name">{{ $produtos->name }}</div>
-                <div class="dots">
-                    <span class="dots-line"></span>
-                </div>
-                <div class="product-price">R$ {{ $produtos->price }}</div>
+
+
+<div class="card-cardapio">
+    @foreach ($categories as $categorie)
+    <div class="py-content">
+        <div>
+            <div class="text-center">
+                <h3 class="title-section text-uppercase category-title">{{ $categorie->name }}</h3>
             </div>
-            <div class="description">{!! $produtos->description !!}</div>
         </div>
-        @endforeach
-       
-       
-
+        <div class="row">
+            @foreach ($categorie->produtos as $produtos)
+            <div class="col-md-6">
+                <div class="product">
+                    <div class="product-name">{{ $produtos->name }}</div>
+                    <div class="dots">
+                        <span class="dots-line"></span>
+                    </div>
+                    <div class="product-price">R$ {{ $produtos->price }}</div>
+                </div>
+                <div class="description">{!! $produtos->description !!}</div>
+            </div>
+            @endforeach
+           
+           
+    
+        </div>
+    
+    
+    
     </div>
-
-
-
+    @endforeach
 </div>
-@endforeach
+
  
 @endsection
