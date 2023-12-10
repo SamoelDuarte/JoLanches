dfeb739863b44c692ad825ac7d48450e4a450d2a
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2023-12-10 09:14:52 -0300

    Update index.blade.php

--- resources/views/admin/sangria/index.blade.php
+++ resources/views/admin/sangria/index.blade.php
@@ -192,6 +192,7 @@
                 "language": {
                     "url": "//cdn.datatables.net/plug-ins/1.10.24/i18n/Portuguese-Brasil.json" // Adiciona o arquivo de localização para português
                 },
+                "order": [[0, 'desc']],
 
             });
 
