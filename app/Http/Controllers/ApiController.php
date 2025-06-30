acc18f65dac05eeb9b62658a4171c407d9014616
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-06-29 21:23:49 -0300

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,33 +11,33 @@ class ApiController extends Controller
     public function getProduto(Request $request)
     {
         $codigo = $request->codigo;
-        if (!$codigo) {
-            return response()->json([
-                'status' => 'erro',
-                'mensagem' => 'Código de barras não enviado.'
-            ], 400);
-        }
+        // if (!$codigo) {
+        //     return response()->json([
+        //         'status' => 'erro',
+        //         'mensagem' => 'Código de barras não enviado.'
+        //     ], 400);
+        // }
 
-        $produto = Product::where('cod_barra', $codigo)->first();
+        // $produto = Product::where('cod_barra', $codigo)->first();
 
-        if (!$produto) {
-            return response()->json([
-                'status' => 'erro',
-                'mensagem' => 'Produto não encontrado.'
-            ], 404);
-        }
+        // if (!$produto) {
+        //     return response()->json([
+        //         'status' => 'erro',
+        //         'mensagem' => 'Produto não encontrado.'
+        //     ], 404);
+        // }
 
-        return response()->json([
-            'status' => 'sucesso',
-            'produto' => [
-                'id' => $produto->id,
-                'nome' => $produto->name,
-                'descricao' => $produto->description,
-                'preco' => number_format($produto->price, 2, ',', '.'),
-                'cod_barra' => $produto->cod_barra,
-                'categoria' => optional($produto->categoria)->nome,
-                'sistema' => $produto->sistema_display, // acessor automático
-            ]
-        ]);
+        // return response()->json([
+        //     'status' => 'sucesso',
+        //     'produto' => [
+        //         'id' => $produto->id,
+        //         'nome' => $produto->name,
+        //         'descricao' => $produto->description,
+        //         'preco' => number_format($produto->price, 2, ',', '.'),
+        //         'cod_barra' => $produto->cod_barra,
+        //         'categoria' => optional($produto->categoria)->nome,
+        //         'sistema' => $produto->sistema_display, // acessor automático
+        //     ]
+        // ]);
     }
 }
