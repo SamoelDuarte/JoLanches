0f39f468efbe2a61e42fe29ca507df12e5a5ace8
SamoelDuarte <samoelduarte@betasolucao.com.br>
2025-06-29 21:02:16 -0300

    Update GitWebhookController.php

--- app/Http/Controllers/GitWebhookController.php
+++ app/Http/Controllers/GitWebhookController.php
@@ -6,22 +6,29 @@ use Illuminate\Http\Request;
 
 class GitWebhookController extends Controller
 {
-   public function handle(Request $request)
-   {
-       $output = [];
-       $returnVar = null;
-       $projectPath = base_path();
+    public function handle(Request $request)
+    {
+        $output = [];
+        $returnVar = null;
+        $projectPath = base_path();
 
-       // Comando para resetar e limpar mudanças locais antes do pull
-       $cmd = "cd {$projectPath} && whoami && /usr/bin/git reset --hard && /usr/bin/git clean -fd && /usr/bin/git pull origin main 2>&1";
+        if (!is_dir($projectPath . '/.git')) {
+            return response()->json([
+                'status' => 'erro',
+                'mensagem' => 'Este diretório não é um repositório Git válido.',
+                'caminho' => $projectPath
+            ], 500);
+        }
 
-       exec($cmd, $output, $returnVar);
+        $cmd = "cd {$projectPath} && whoami && /usr/bin/git reset --hard && /usr/bin/git clean -fd && /usr/bin/git pull origin main 2>&1";
 
-       return response()->json([
-           'executed_as' => exec('whoami'),
-           'executed_command' => $cmd,
-           'output' => $output,
-           'return_var' => $returnVar
-       ]);
-   }
+        exec($cmd, $output, $returnVar);
+
+        return response()->json([
+            'executed_as' => exec('whoami'),
+            'executed_command' => $cmd,
+            'output' => $output,
+            'return_var' => $returnVar
+        ]);
+    }
 }
